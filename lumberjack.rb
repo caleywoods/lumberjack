d2f1caa09a9f477a0ea6061986f46a292aa30e40
Caley Woods <caley.w@gmail.com>
2012-12-08 00:07:33 -0600

    testing

--- lumberjack.rb
+++ lumberjack.rb
@@ -6,13 +6,14 @@ require 'haml'       #view templates
 
 module Lumberjack
   class App < Guillotine::App
-  	uri     = URI.parse(ENV["REDISTOGO_URL"])
-    redis   = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
-    adapter = Guillotine::Adapters::RedisAdapter.new(redis)
-    set :service => Guillotine::Service.new(adapter)
+    # uri     = URI.parse(ENV["REDISTOGO_URL"])
+    # redis   = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
+    # adapter = Guillotine::Adapters::RedisAdapter.new(redis)
+    # set :service => Guillotine::Service.new(adapter)
 
     get '/' do
     	:haml index
     end
+
   end
 end
