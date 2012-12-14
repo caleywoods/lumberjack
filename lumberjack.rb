a3920e1573b15c24ae654695229707f6f04b4173
Caley Woods <caley.w@gmail.com>
2012-12-13 20:21:06 -0600

    moved JS files to JS folder

--- lumberjack.rb
+++ lumberjack.rb
@@ -3,10 +3,10 @@ require 'redis'      #For redistogo on Heroku
 
 module Lumberjack
   class App < Guillotine::App
-    uri     = URI.parse(ENV["REDISTOGO_URL"])
-    redis   = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
-    adapter = Guillotine::Adapters::RedisAdapter.new(redis)
-    set :service => Guillotine::Service.new(adapter)
+    # uri     = URI.parse(ENV["REDISTOGO_URL"])
+    # redis   = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
+    # adapter = Guillotine::Adapters::RedisAdapter.new(redis)
+    # set :service => Guillotine::Service.new(adapter)
 
     get '/' do
       haml :index
